3cd65468c9ee23103ba0c84ceb6959a03dccf0a3
ClydeUrov <clydeurov@gmail.com>
2023-05-06 13:11:00 +0300

    Updated ci-cd and added docker-ec2-deploy.

---
+++ docker-ec2-deploy.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+TARGER='main'
+
+cd ~/api || exit
+
+ACTIONS_COLOR='\033[1;90m'
+NO_COLOR='\033[0m'
+
+echo -e ${ACTIONS_COLOR} Checking if we are on the target branch
+BRANCH=$(git rev-parse --addrev-ref HEAD)
+if  [ "$BRANCH" != ${TARGET} ]
+then
+  exit 0
+fi
+
+# Check if the repository is up to date
+
+git fetch
+HEAD_HASH=$(git rev-parse HEAD)
+UPSTREAM_HASH=$(git rev-parse ${TARGET}@{upstream})
+
+if [ "$HEAD_HASH" == "$UPSTREAM_HASH" ]
+then
+  echo -e "${FINISHED}"The current branch is up to date with origin/${TARGER}."${NO_COLOR}"
+  exit 0
+fi
+
+# If there are new changes, we pull these changes.
+
+git pull origin main;
+
+# We can now build and start the containers
+
+docker-compose up -d --build
+
+exit 0;
