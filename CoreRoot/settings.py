2e8223090722312f0bf208d96def0fdfefba687e
ClydeUrov <clydeurov@gmail.com>
2023-05-08 19:10:10 +0300

    Updated settings nginx

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -154,10 +154,7 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
+CORS_ALLOWED_ORIGINS = os.getenv("CORE_ALLOWED_ORIGINS", "").split(",")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "uploads"
