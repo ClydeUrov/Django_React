f4279e71d46403cbf7b7645bd00e4f524307694b
ClydeUrov <clydeurov@gmail.com>
2023-07-26 10:14:39 +0300

    Fixed overflow and navigation.

--- social-media-app/src/pages/ChatRoom.jsx
+++ social-media-app/src/pages/ChatRoom.jsx
@@ -13,7 +13,7 @@ function ChatRoom() {
     const { roomId } = useParams();
 
     const rooms = useSWR(`/room/`, fetcher);
-    const chats = useSWR(roomId ? `/room/${roomId}/chat/` : null, fetcher);
+    const chats = useSWR(`/room/${roomId}/chat/`, fetcher);
 
     if (!roomId) {
         return <div>Choose a room, please</div>;
@@ -27,35 +27,49 @@ function ChatRoom() {
                     overflow: hidden;
                     max-height: 450px;
                 }
-
                 .rooms-overflow:hover {
                     overflow: auto;
                     transition: 0.5s;
                 }
+                ::-webkit-scrollbar {
+                    width: 10px;
+                }
+                ::-webkit-scrollbar-track {
+                    box-shadow: inset 0 0 5px grey;
+                    border-radius: 10px;
+                    transition: 0.5s;
+                }
+                ::-webkit-scrollbar-thumb {
+                    background: rgb(37, 152, 230);
+                    border-radius: 10px;
+                    transition: 0.5s;
+                }
             `}
             </style>
-            <h1>Chat Room</h1>
+            
             <Row className="justify-content-center">
-                <Col sm={3} className='me-3'>
-                    <h2>Room Details</h2>
-                    <div className="rooms-overflow">
-                        {rooms.data && rooms.data.results.map((room, index) => (
+                <Col sm={3} className=''>
+                    <h2>Rooms</h2>
+                    <div className="rooms-overflow justify-content-center">
+                        {rooms.data && rooms.data.results.map((room) => (
                             <Link to={`/room/${room.id}/`} key={room.id} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                <Room room={room} refresh={rooms.mutate} />
+                                <Room room={room} refresh={rooms.mutate} currentRoom={roomId} />
                             </Link>
                         ))}
                     </div>
                 </Col>
                 <Col sm={7}  className="">
-                    <h2>Chat Messages</h2>
-                    <div className="overflow-auto border bg-white rounded-3 py-4" style={{maxHeight: '450px', minHeight: '200px'}}>
-                        {chats.data && chats.data.results.map((chat, index) => (
-                            <div key={index}>
-                                <Chat roomId={roomId} chat={chat} refresh={chat.mutate} />
-                            </div>
-                        ))}
+                    <h2>Messages</h2>
+                    <div className="border bg-white rounded-3 pt-4" >
+                        <div className="overflow-auto" style={{maxHeight: '450px'}}>
+                            {chats.data && chats.data.results.map((chat, index) => (
+                                <div key={index}>
+                                    <Chat roomId={roomId} chat={chat} refresh={chats.mutate} />
+                                </div>
+                            ))}
+                        </div>
+                        <SendMessage roomId={roomId} refresh={chats.mutate} />
                     </div>
-                    <SendMessage roomId={roomId} refresh={chats.mutate} />
                 </Col>
             </Row>
         </Layout>
